06674081a9be8bed5dd0f3d04af59d63b324612d
Allan Tavares <allan.nunes@bandtec.com.br>
2019-09-12 18:49:46 -0300

    (Teste) Teste ViewController

---
+++ src/main/java/br/com/bandtec/TioVan/Controller/ViewController.java
@@ -0,0 +1,14 @@
+package br.com.bandtec.TioVan.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+	
+}
