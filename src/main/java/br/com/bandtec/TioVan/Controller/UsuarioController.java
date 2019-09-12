41e2276b05cea243bb494037e47e2e8220a0c918
Allan Tavares <allan.nunes@bandtec.com.br>
2019-09-12 18:24:34 -0300

    (Teste) Criação do método para cadastrar usuário no MongoDB

--- src/main/java/br/com/bandtec/TioVan/Controller/UsuarioController.java
+++ src/main/java/br/com/bandtec/TioVan/Controller/UsuarioController.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bson.Document;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @RestController
 public class UsuarioController {
@@ -75,6 +82,29 @@ public class UsuarioController {
     @PostMapping("/cadastrar_usuario")
     public void cadastrarUsuarioMongo(@RequestBody int idUsuario, String strNomeUsuario, 
     		String strLoginUsuario, String strSenhaUsuario ) {
+    	
+		MongoClient mongoClient = MongoClients.create("mongodb+srv://admtiovan:tiovanapp2019@tiovan-hfphq.azure.mongodb.net/test?retryWrites=true&w=majority");
+		MongoDatabase database = mongoClient.getDatabase("dbTioVan");
+
+		System.out.println(database.getName());
+		
+		MongoCollection<Document> collection = database.getCollection("tbUsuarios");
+		
+		Document doc = new Document("idUsuario", idUsuario)
+                .append("strNomeUsuario", strNomeUsuario)
+                .append("strLoginUsuario", strLoginUsuario)
+                .append("strSenhaUsuario", strSenhaUsuario);
+	 
+		collection.insertOne(doc);
+		
+		/*BasicDBObject query = new BasicDBObject();
+		
+		query.put("idUsuario",1);
+		
+		System.out.println(collection.find(query).first().toJson());*/
+		
+
+		
     	
     }
     
