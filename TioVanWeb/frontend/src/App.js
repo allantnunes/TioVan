f3f54c08cc6888bd33ba7b436c1baaf4b9babcbf
Cybelle Souza <cybelle.souza@la.logicalis.com>
2019-10-15 17:35:04 -0300

    Tela de Login

--- TioVanWeb/frontend/src/App.js
+++ TioVanWeb/frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import logo from './img/LogoPortal.PNG'
 
 function App() {
   return (
-    <h1>Hello Word
-</h1>
+    <div className="container">
+      <img src={logo} />
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" placeholder="E-mail" />
+        </form>
+      </div>
+    </div>
+
   );
 }
 
