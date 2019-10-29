ed6491fdacfabf0c4dab24df609344edc33e7545
Lucas AFerrer <502100@soa.ad.pwcinternal.com>
2019-10-29 08:41:11 -0300

    arquivo web.config

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import "./web.config";
 ReactDOM.render(<App />, document.getElementById('root'));
