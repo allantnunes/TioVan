0c40c85ec55430b54c5722c759adad7c18654c48
DanieldSRufino <daniel.rufino15@gmail.com>
2019-11-07 21:20:13 -0300

    Autenticação de Login v2

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,30 +2,30 @@ import React, { useState } from 'react';
 import api from '../../services/api';
 import '../login/App.css';
 import Navbar from '../snippets/navbar';
-import { Link, withRouter } from "react-router-dom";
-// import { login } from "../../services/auth";
+import { Link } from "react-router-dom";
+
 
 export default function Login({ history }) {
 
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const login = {email: email, senha: senha}
+    const [email, getEmail] = useState('');
+    const [senha, getSenha] = useState('');
+    const login = { email: email, senha: senha }
     var _id = "";
-    async function handleSubmit(event) {
+    async function handleLogin(event) {
         event.preventDefault();
 
         const response = await api.post('https://tiovan.herokuapp.com/motorista/login', login)
-        .then(response => {
-            console.log(response);
-            _id = response.data;
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(response => {
+                console.log(response);
+                _id = response.data;
+            })
+            .catch(error => {
+                console.log(error)
+            })
 
 
         console.log(_id);
-        localStorage.setItem('user', _id);
+        // localStorage.setItem('user', _id);
 
         history.push('/motorista/portal');
 
@@ -33,29 +33,28 @@ export default function Login({ history }) {
     return (
         <>
             <Navbar />
-            
+
             <div className="containerLogin">
                 <div className="contentLogin">
                     <div className="logo" />
                     <center><h1>Login</h1></center>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleLogin}>
                         <label htmlFor="email">E-mail</label>
                         <input type="email" id="email" placeholder="E-mail"
                             value={email}
-                            onChange={event => setEmail(event.target.value)} />
+                            onChange={event => getEmail(event.target.value)} />
                         <label htmlFor="senha">Senha</label>
                         <input type="password" id="senha" placeholder="Digite a senha"
                             value={senha}
-                            onChange={event => setSenha(event.target.value)} />
-                    
+                            onChange={event => getSenha(event.target.value)} />
 
-                    <center><p>Não é cadastrado? clique <Link to="/motorista/cadastro">aqui</Link></p></center>
 
-                    <button type="submit" className="btnLogin">Entrar</button>
+                        <center><p>Não é cadastrado? clique <Link to="/motorista/cadastro">aqui</Link></p></center>
+
+                        <button type="submit" className="btnLogin">Entrar</button>
                     </form>
                 </div>
             </div>
-           
         </>
     );
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,5 @@
-export const isAuthenticated = ()  => localStorage.getItem('user') != null;;
-
+export const isAuthenticated = ()  => localStorage.getItem('user') != null;
+export const login = _id => {
+    localStorage.setItem('user', _id);
+  };
 
